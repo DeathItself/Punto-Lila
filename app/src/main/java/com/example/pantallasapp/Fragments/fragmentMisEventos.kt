b76828a81211239f30d976fa8455cbeb1c35b727
Soufian Ahlal <ahlal.soufian@alumnat.copernic.cat>
2022-12-13 20:14:09 +0100

    v18

--- app/src/main/java/com/example/pantallasapp/Fragments/fragmentMisEventos.kt
+++ app/src/main/java/com/example/pantallasapp/Fragments/fragmentMisEventos.kt
@@ -1,11 +1,14 @@
 package com.example.pantallasapp.Fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pantallasapp.R
+import com.example.pantallasapp.databinding.FragmentEventosBinding
+import com.example.pantallasapp.databinding.FragmentMisEventosBinding
 
 
 private const val ARG_PARAM1 = "param1"
@@ -16,6 +19,9 @@ class fragmentMisEventos : Fragment() {
 
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentMisEventosBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +35,29 @@ class fragmentMisEventos : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mis_eventos, container, false)
+        inflater.inflate(R.layout.fragment_mis_eventos, container, false)
+        _binding = FragmentMisEventosBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bundle = arguments
+
+        if ( bundle == null){
+            Log.d("Confirmation", "Mis eventos no ha recibido info" )
+            return
+        }
+
+        val args = fragmentMisEventosArgs.fromBundle(bundle)
+
+      //  if (args.name.isNullOrBlank()){
+      //      binding.EventosApuntado.text = getString(R.id.boton1)
+
+      //  } else {
+      //      binding.EventosApuntado.text = args.name
+      //  }
     }
 
     companion object {
